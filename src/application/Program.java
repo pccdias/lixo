337a297c96651989ed3d3c0f3e032952b876493f
Paulo Cesar C Dias <pccdias@gmail.com>
2019-08-19 16:15:29 -0300

    Teste de git

---
+++ src/application/Program.java
@@ -0,0 +1,71 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException{
+		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyy");
+		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyy HH:mm:ss");
+		SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyy HH:mm:ss");
+		sdf3.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		Date x1 = new Date();
+		Date x2 = new Date(System.currentTimeMillis());
+		Date x3 = new Date(0L);
+		Date x4 = new Date(1000L*60L*60L*5L);
+				
+		
+		Date y1 = sdf1.parse("19/08/2019");
+		Date y2 = sdf2.parse("19/08/2019 15:05:23");
+		Date y3 = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		
+		System.out.println("y1:" + sdf2.format(y1));
+		System.out.println("y1:" + sdf2.format(y2));	
+		System.out.println("x1:" + sdf2.format(x1));
+		System.out.println("x2:" + sdf2.format(x2));
+		System.out.println("x3:" + sdf2.format(x3));
+		System.out.println("x4:" + sdf2.format(x4));
+		System.out.println("y3:" + sdf2.format(y3));
+		
+		System.out.println("------------------------------------");
+		System.out.println("y1:" + sdf3.format(y1));
+		System.out.println("y1:" + sdf3.format(y2));	
+		System.out.println("x1:" + sdf3.format(x1));
+		System.out.println("x2:" + sdf3.format(x2));
+		System.out.println("x3:" + sdf3.format(x3));
+		System.out.println("x4:" + sdf3.format(x4));
+		System.out.println("y3:" + sdf3.format(y3));
+		
+		System.out.println("----------CALENDAR---------------");
+		Date d = Date.from(Instant.parse("2019-06-25T15:42:07Z"));
+		System.out.println(sdf2.format(d));
+		
+		Calendar cal = Calendar.getInstance();
+		
+		cal.setTime(d);
+		cal.add(Calendar.HOUR_OF_DAY, 4);
+		d = cal.getTime();
+		int minutes = cal.get(Calendar.MINUTE);
+		int month = cal.get(Calendar.MONTH) + 1;
+		System.out.println(sdf2.format(d));
+		System.out.println("Minutes: " + minutes);
+		System.out.println("Month: " + month);
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
